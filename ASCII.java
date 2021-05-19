5c6933de8492e7373140be62b15d06ec8ca17f05
NabilFayez <68347642+NabilFayez@users.noreply.github.com>
2021-05-19 04:38:23 +0200

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,53 @@
+package com.a;
+
+
+
+
+import java.util.Scanner ;
+
+public class Main
+
+{
+
+  public static void main(String[] args) 
+
+  {
+
+   Scanner input= new Scanner(System.in);
+
+     System.out.print("Enter ASCII value==> ");
+
+	while (input.hasNext()){
+
+	String str = input.next();	
+    System.out.println("The value " + str + " is equal to ==>" + (char)Integer.parseInt(str));};
+
+   
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+  }
+
+} 
